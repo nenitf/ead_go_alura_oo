51afe21c8660c7050c8912acaae881a5bc2e8484
Felipe Silva <felipe.dacs@gmail.com>
2021-04-15 00:33:33 -0300

    :tv: add aula 3

--- main.go
+++ main.go
@@ -10,5 +10,14 @@ type ContaCorrente struct {
 }
 
 func main() {
-	fmt.Println(ContaCorrente{})
+	//var contaDoGuilherme ContaCorrente = ContaCorrente{}
+	// contaDoGuilherme := ContaCorrente{}
+
+	contaDoGuilherme := ContaCorrente{titular: "Guilherme",
+		numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
+
+	contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200}
+
+	fmt.Println(contaDoGuilherme)
+	fmt.Println(contaDaBruna)
 }
