745069b56fcbcbd3c826b5003fb40a016a1b3e5a
Felipe Silva <felipe.dacs@gmail.com>
2021-04-15 00:27:46 -0300

    :tv: add aula 1

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # ead_go_alura_oo
 
 > Projeto referente a [este](https://www.alura.com.br/curso-online-go-lang-oo) curso.
+
+## Execução dos scripts
+
+- Terminal com `go run main.php`
+- Vim com `:!go run main.go` (`:!!` repete o comando anterior)
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var titular2 string = "Luciene"
+	var numeroAgencia2 int = 555
+	var numeroConta2 int = 111333
+	var saldo2 float64 = 200
+
+	fmt.Println(titular2, numeroAgencia2, numeroConta2, saldo2)
+
+}
