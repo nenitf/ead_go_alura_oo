e3f1f0f4343947e5b59fd3158320df070dc13b24
Felipe Silva <felipe.dacs@gmail.com>
2021-04-15 00:45:31 -0300

    :tv: add aula 5

--- main.go
+++ main.go
@@ -13,18 +13,37 @@ func main() {
 	//var contaDoGuilherme ContaCorrente = ContaCorrente{}
 	// contaDoGuilherme := ContaCorrente{}
 
+	// contaDoGuilherme := ContaCorrente{titular: "Guilherme",
+	// 	numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
+
+	// contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200}
+
+	// fmt.Println(contaDoGuilherme)
+	// fmt.Println(contaDaBruna)
+
+	// var contaDaCris *ContaCorrente // pq asterisco?
+	// contaDaCris = new(ContaCorrente)
+	// contaDaCris.titular = "Cris"
+
+	// fmt.Println(contaDaCris) // ponteiro
+	// fmt.Println(*contaDaCris) // valor
+	// fmt.Println(&contaDaCris) // endereço de memória
+
 	contaDoGuilherme := ContaCorrente{titular: "Guilherme",
 		numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
+	contaDoGuilherme2 := ContaCorrente{titular: "Guilherme",
+		numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
 
-	contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200}
+	fmt.Println(contaDoGuilherme == contaDoGuilherme2) // true (compara valores)
 
-	fmt.Println(contaDoGuilherme)
-	fmt.Println(contaDaBruna)
-
-	var contaDaCris *ContaCorrente // pq asterisco?
+	var contaDaCris *ContaCorrente
 	contaDaCris = new(ContaCorrente)
 	contaDaCris.titular = "Cris"
 
-	fmt.Println(contaDaCris)
-	fmt.Println(*contaDaCris)
+	var contaDaCris2 *ContaCorrente
+	contaDaCris2 = new(ContaCorrente)
+	contaDaCris2.titular = "Cris"
+
+	fmt.Println(contaDaCris == contaDaCris2)   // false (compara endereços de memória)
+	fmt.Println(*contaDaCris == *contaDaCris2) // true (compara valores)
 }
