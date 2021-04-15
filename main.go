17cdd94cbe931e7ac9b9ac7e1f1c05e7b04ad623
Felipe Silva <felipe.dacs@gmail.com>
2021-04-15 00:30:37 -0300

    :tv: add aula 2

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import "fmt"
 
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
+
 func main() {
-	var titular2 string = "Luciene"
-	var numeroAgencia2 int = 555
-	var numeroConta2 int = 111333
-	var saldo2 float64 = 200
-
-	fmt.Println(titular2, numeroAgencia2, numeroConta2, saldo2)
-
+	fmt.Println(ContaCorrente{})
 }
