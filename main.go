1081755582e9cb439c59f0fcbb3c96422903f1b4
Felipe Silva <felipe.dacs@gmail.com>
2021-04-15 00:51:11 -0300

    :tv: add aula 6

--- main.go
+++ main.go
@@ -9,41 +9,23 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
 func main() {
-	//var contaDoGuilherme ContaCorrente = ContaCorrente{}
-	// contaDoGuilherme := ContaCorrente{}
+	contaDaSilvia := ContaCorrente{}
+	contaDaSilvia.titular = "Silvia"
+	contaDaSilvia.saldo = 500
 
-	// contaDoGuilherme := ContaCorrente{titular: "Guilherme",
-	// 	numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
+	fmt.Println(contaDaSilvia.saldo)
 
-	// contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200}
-
-	// fmt.Println(contaDoGuilherme)
-	// fmt.Println(contaDaBruna)
-
-	// var contaDaCris *ContaCorrente // pq asterisco?
-	// contaDaCris = new(ContaCorrente)
-	// contaDaCris.titular = "Cris"
-
-	// fmt.Println(contaDaCris) // ponteiro
-	// fmt.Println(*contaDaCris) // valor
-	// fmt.Println(&contaDaCris) // endereço de memória
-
-	contaDoGuilherme := ContaCorrente{titular: "Guilherme",
-		numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
-	contaDoGuilherme2 := ContaCorrente{titular: "Guilherme",
-		numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
-
-	fmt.Println(contaDoGuilherme == contaDoGuilherme2) // true (compara valores)
-
-	var contaDaCris *ContaCorrente
-	contaDaCris = new(ContaCorrente)
-	contaDaCris.titular = "Cris"
-
-	var contaDaCris2 *ContaCorrente
-	contaDaCris2 = new(ContaCorrente)
-	contaDaCris2.titular = "Cris"
-
-	fmt.Println(contaDaCris == contaDaCris2)   // false (compara endereços de memória)
-	fmt.Println(*contaDaCris == *contaDaCris2) // true (compara valores)
+	fmt.Println(contaDaSilvia.Sacar(400))
+	fmt.Println(contaDaSilvia.saldo)
 }
