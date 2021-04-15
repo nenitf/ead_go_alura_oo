d13a75e89cf561ec33e12cceca22c09b3ab77079
Felipe Silva <felipe.dacs@gmail.com>
2021-04-15 01:28:02 -0300

    :tv: add aula 13

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/nenitf/ead_go_alura_oo/clientes"
 	"github.com/nenitf/ead_go_alura_oo/contas"
 )
 
 func main() {
-	clienteBruno := clientes.Titular{"Bruno", "123.123.123.12", "Desenvolvedor"}
-	contaDoBruno := contas.ContaCorrente{clienteBruno, 123, 123456, 100}
-	fmt.Println(contaDoBruno)
+	contaExemplo := contas.ContaCorrente{}
+	contaExemplo.Depositar(-100)
+
+	fmt.Println(contaExemplo.ObterSaldo())
 }
